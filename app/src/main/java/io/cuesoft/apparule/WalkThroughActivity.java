c5e7ab012ac2a5debf15d4f6c2edb6032d31851e
Abayomi <Oladinifolawiyo@gmail.com>
2019-01-06 22:27:37 +0100

    eeqeeeeeeeeeeeeeeeeeeeeeeeeeirst

--- app/src/main/java/io/cuesoft/apparule/WalkThroughActivity.java
+++ app/src/main/java/io/cuesoft/apparule/WalkThroughActivity.java
@@ -1,13 +1,18 @@
 package io.cuesoft.apparule;
 
-import android.preference.PreferenceManager;
+
+import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -17,7 +22,7 @@ public class WalkThroughActivity extends AppCompatActivity {
     PreferenceManager preferenceManager;
     LinearLayout Layout_bars;
     int[] screens;
-    TextView[] bototmBars;
+    TextView[] bottomBars;
     Button Skip,Next;
     MyViewPagerAdapter myvpAdapter;
     ViewPager vp;
@@ -28,9 +33,97 @@ public class WalkThroughActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walk_through);
 
+
+        vp =findViewById(R.id.view_pager);
+        Layout_bars = findViewById(R.id.layoutBars);
+        Skip =  findViewById(R.id.skip);
+        Next = findViewById(R.id.next);
+        myvpAdapter = new MyViewPagerAdapter();
+        vp.setAdapter(myvpAdapter);
+
+        preferenceManager = new PreferenceManager(this);
+        vp.addOnPageChangeListener(viewPagerPageChangeListener);
+
+        if (!preferenceManager.FirstLaunch()) {
+            launchMain();
+            finish();
+        }
+
+        screens = new int[]{
+                R.layout.intro_screen1,
+                R.layout.intro_screen2,
+                R.layout.intro_screen3
+        };
+        ColoredBars(0);
     }
 
 
+
+    public void next(View v) {
+        int i = getItem(+1);
+        if (i < screens.length) {
+            vp.setCurrentItem(i);
+        } else {
+            launchMain();
+        }
+    }
+
+
+    public void skip(View view) {
+        launchMain();
+    }
+
+    private void ColoredBars(int thisScreen) {
+        int[] colorsInactive = getResources().getIntArray(R.array.dot_on_page_not_active);
+        int[] colorsActive = getResources().getIntArray(R.array.dot_on_page_active);
+        bottomBars = new TextView[screens.length];
+
+        Layout_bars.removeAllViews();
+        for (int i = 0; i < bottomBars.length; i++) {
+            bottomBars[i] = new TextView(this);
+            bottomBars[i].setTextSize(100);
+            bottomBars[i].setText(Html.fromHtml("¯"));
+            Layout_bars.addView(bottomBars[i]);
+            bottomBars[i].setTextColor(colorsInactive[thisScreen]);
+        }
+        if (bottomBars.length > 0)
+            bottomBars[thisScreen].setTextColor(colorsActive[thisScreen]);
+    }
+
+    private int getItem(int i) {
+        return vp.getCurrentItem() + i;
+    }
+
+    private void launchMain() {
+        preferenceManager.setFirstTimeLaunch(false);
+        startActivity(new Intent(WalkThroughActivity.this, HomeActivity.class));
+        finish();
+    }
+
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(int position) {
+            ColoredBars(position);
+            if (position == screens.length - 1) {
+                Next.setText("start");
+                Skip.setVisibility(View.GONE);
+            } else {
+                Next.setText("Next");
+                Skip.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+    };
 
     public class MyViewPagerAdapter extends PagerAdapter{
          private LayoutInflater inflater;
@@ -39,15 +132,28 @@ public class WalkThroughActivity extends AppCompatActivity {
 
          }
 
-       @Override
-       public int getCount() {
-          return 0;
-      }
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = inflater.inflate(screens[position], container, false);
+            container.addView(view);
+            return view;
+        }
 
-       @Override
-      public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-          return false;
-      }
- }
+        @Override
+        public int getCount() {
+            return screens.length;
+        }
 
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View v = (View) object;
+            container.removeView(v);
+        }
+
+        @Override
+        public boolean isViewFromObject(View v, Object object) {
+            return v == object;
+        }
+    }
 }
