7990b3afbe74bba82590cfd95fd010e8c34a5734
Abayomi <Oladinifolawiyo@gmail.com>
2019-01-10 21:52:42 +0100

    Added Walkthrough sliders Used external library for sliders

--- app/src/main/java/io/cuesoft/apparule/views/WalkThroughActivity.java
+++ app/src/main/java/io/cuesoft/apparule/views/WalkThroughActivity.java
@@ -1,160 +1,83 @@
 package io.cuesoft.apparule.views;
 
 
-import android.content.Context;
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Html;
-import android.view.LayoutInflater;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+
+import com.github.paolorotolo.appintro.AppIntro;
 
 import io.cuesoft.apparule.R;
-import io.cuesoft.apparule.data.PreferenceManager;
+import io.cuesoft.apparule.helper.WalkThroughHelper;
 
-public class WalkThroughActivity extends AppCompatActivity {
-
-    PreferenceManager preferenceManager;
-    LinearLayout Layout_bars;
-    int[] screens;
-    TextView[] bottomBars;
-    Button Skip,Next;
-    MyViewPagerAdapter myvpAdapter;
-    ViewPager vp;
-
+public class WalkThroughActivity extends AppIntro {
+    private WalkThroughHelper preferenceHelper;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_walk_through);
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            //setContentView(R.layout.activity_main);
 
+            preferenceHelper = new WalkThroughHelper(this);
 
-        vp =findViewById(R.id.view_pager);
-        Layout_bars = findViewById(R.id.layoutBars);
-        Skip =  findViewById(R.id.skip);
-        Next = findViewById(R.id.next);
-        myvpAdapter = new MyViewPagerAdapter();
-        vp.setAdapter(myvpAdapter);
-
-        preferenceManager = new PreferenceManager(this);
-        vp.addOnPageChangeListener(viewPagerPageChangeListener);
-
-        if (!preferenceManager.FirstLaunch()) {
-            launchMain();
-            finish();
-        }
-
-        screens = new int[]{
-                R.layout.intro_screen1,
-                R.layout.intro_screen2,
-                R.layout.intro_screen3
-        };
-        ColoredBars(0);
-    }
-
-
-
-    public void next(View v) {
-        int i = getItem(+1);
-        if (i < screens.length) {
-            vp.setCurrentItem(i);
-        } else {
-            launchMain();
-        }
-    }
-
-
-    public void skip(View view) {
-        launchMain();
-    }
-
-    private void ColoredBars(int thisScreen) {
-        int[] colorsInactive = getResources().getIntArray(R.array.dot_on_page_not_active);
-        int[] colorsActive = getResources().getIntArray(R.array.dot_on_page_active);
-        bottomBars = new TextView[screens.length];
-
-        Layout_bars.removeAllViews();
-        for (int i = 0; i < bottomBars.length; i++) {
-            bottomBars[i] = new TextView(this);
-            bottomBars[i].setTextSize(100);
-            bottomBars[i].setText(Html.fromHtml("¯"));
-            Layout_bars.addView(bottomBars[i]);
-            bottomBars[i].setTextColor(colorsInactive[thisScreen]);
-        }
-        if (bottomBars.length > 0)
-            bottomBars[thisScreen].setTextColor(colorsActive[thisScreen]);
-    }
-
-    private int getItem(int i) {
-        return vp.getCurrentItem() + i;
-    }
-
-    private void launchMain() {
-        preferenceManager.setFirstTimeLaunch(false);
-        startActivity(new Intent(WalkThroughActivity.this, HomeActivity.class));
-        finish();
-    }
-
-    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
-
-        @Override
-        public void onPageSelected(int position) {
-            ColoredBars(position);
-            if (position == screens.length - 1) {
-                Next.setText("start");
-                Skip.setVisibility(View.GONE);
-            } else {
-                Next.setText("Next");
-                Skip.setVisibility(View.VISIBLE);
+            if (preferenceHelper.getIntro().equals("no")) {
+                Intent intent = new Intent(WalkThroughActivity.this, HomeActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                this.finish();
             }
+
+            addSlide(new Intro_screen1());  //extend AppIntro and comment setContentView
+            addSlide(new Intro_screen2());
+            addSlide(new Intro_screen3());
+
         }
 
         @Override
-        public void onPageScrolled(int arg0, float arg1, int arg2) {
+        public void onSkipPressed (Fragment currentFragment){
+            super.onSkipPressed(currentFragment);
 
+            preferenceHelper.putIntro("no");
+            Intent intent = new Intent(WalkThroughActivity.this, HomeActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            this.finish();
         }
 
         @Override
-        public void onPageScrollStateChanged(int arg0) {
+        public void onDonePressed (Fragment currentFragment){
+            super.onDonePressed(currentFragment);
 
-        }
-    };
-
-    public class MyViewPagerAdapter extends PagerAdapter{
-         private LayoutInflater inflater;
-
-       public MyViewPagerAdapter(){
-
-         }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View view = inflater.inflate(screens[position], container, false);
-            container.addView(view);
-            return view;
+            preferenceHelper.putIntro("no");
+            Intent intent = new Intent(WalkThroughActivity.this, HomeActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            this.finish();
         }
 
         @Override
-        public int getCount() {
-            return screens.length;
+        public void onSlideChanged (@Nullable Fragment oldFragment, @Nullable Fragment newFragment){
+            super.onSlideChanged(oldFragment, newFragment);
+            // Do something when the slide changes.
         }
 
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            View v = (View) object;
-            container.removeView(v);
-        }
+        @Override  // this method is used for removing top and bottom navbars(fullscreen)
+        public void onWindowFocusChanged ( boolean hasFocus){
+            super.onWindowFocusChanged(hasFocus);
+            if (hasFocus) {
+                getWindow().getDecorView().setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            }
 
-        @Override
-        public boolean isViewFromObject(View v, Object object) {
-            return v == object;
         }
     }
-}
+
