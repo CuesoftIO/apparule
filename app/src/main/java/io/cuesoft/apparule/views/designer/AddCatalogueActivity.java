8a7a24d7491004ee717d77cf9a859cd0b08cce49
Abayomi <Oladinifolawiyo@gmail.com>
2019-02-04 17:27:01 +0100

    Added Catalogue and profile Activity

---
+++ app/src/main/java/io/cuesoft/apparule/views/designer/AddCatalogueActivity.java
@@ -0,0 +1,15 @@
+package io.cuesoft.apparule.views.designer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.cuesoft.apparule.R;
+
+public class AddCatalogueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_catalogue);
+    }
+}
